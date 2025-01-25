747ea680d9cfffb5d886c8eef42c9fecd4ca3dbd
ashishk15678 <147980956+ashishk15678@users.noreply.github.com>
2025-01-25 15:56:09 +0530

    Day 6

--- a.py
+++ a.py
@@ -1,10 +1,31 @@
-n1 = int(input("n1: "))
-n2 = int(input("n2: "))
-i = 1
-while(i<=10):   # Number of lines
-    j = n1
-    while(j<=n2):
-        print("(",j,"*",i,"=",i*j,")",end="")
-        j+=1
-    print("\n")
-    i+=1
+# Calculator using functions
+
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+
+
+a = int(input("Write your choice: \n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division"))
+if (a!=1 or a!=2 or a!=3 or a!=4):
+    print("ERROR : Wrong choice")
+    exit(-1)
+b = int(input("First number: "))
+c = int(input("Second number: "))
+res = 0
+if(a == 1):
+    res = add(b,c)
+elif a == 2:
+    res = sub(b,c)
+elif a == 3:
+    res = mul(b,c)
+elif a == 4:
+    res = div(b,c)
+else :
+    print("ERROR : Wronng Choice")
+    exit(-1)
+print("Result:",res)
